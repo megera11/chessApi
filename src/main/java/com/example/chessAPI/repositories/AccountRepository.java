997eefffc51c616830b3d31cc10266c664ebaafb
megera11 <marekhajduczenia11@gmail.com>
2021-04-18 16:02:32 +0200

    //

--- src/main/java/com/example/chessAPI/repositories/AccountRepository.java
+++ src/main/java/com/example/chessAPI/repositories/AccountRepository.java
@@ -3,10 +3,11 @@ package com.example.chessAPI.repositories;
 import com.example.chessAPI.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-
+@Component("accountRepository")
 public interface AccountRepository extends JpaRepository<Account,Long> {
 
     List<Account> findAllByLogin(String login);
