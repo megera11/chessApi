170bbe7074d725cdea8300254d1478cc8173fea9
megera11 <marekhajduczenia11@gmail.com>
2021-04-18 16:10:28 +0200

    //

--- src/main/java/com/example/chessAPI/annotations/UniqueLoginValidator.java
+++ src/main/java/com/example/chessAPI/annotations/UniqueLoginValidator.java
@@ -6,9 +6,10 @@ import javax.validation.ConstraintValidatorContext;
 import com.example.chessAPI.models.Account;
 import com.example.chessAPI.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+@Component
 public class UniqueLoginValidator implements ConstraintValidator<UniqueLogin, String>{
     @Autowired
     private AccountRepository accountRepository;
