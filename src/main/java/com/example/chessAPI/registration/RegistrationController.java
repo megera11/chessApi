4691fe622fdbc08c0d650691697b4da0e8673aa0
megera11 <marekhajduczenia11@gmail.com>
2021-04-18 12:32:26 +0200

    //

--- src/main/java/com/example/chessAPI/registration/RegistrationController.java
+++ src/main/java/com/example/chessAPI/registration/RegistrationController.java
@@ -3,6 +3,7 @@ package com.example.chessAPI.registration;
 import com.example.chessAPI.models.Account;
 import com.example.chessAPI.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
@@ -18,6 +19,7 @@ public class RegistrationController {
     AccountRepository accountRepository;
 
     @PostMapping
+    @ResponseStatus(value= HttpStatus.OK)
     public void register(@Valid @RequestBody Account account){
         accountRepository.save(account);
     }
