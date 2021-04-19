cbd577e56ca5fa36e8fc6edbd88e63d2d12e56f2
megera11 <marekhajduczenia11@gmail.com>
2021-04-19 20:08:08 +0200

    //

--- src/main/java/com/example/chessAPI/ChessApiApplication.java
+++ src/main/java/com/example/chessAPI/ChessApiApplication.java
@@ -10,7 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class ChessApiApplication {
 
 	@Bean
-	public PasswordEncoder encoder(){
+	public BCryptPasswordEncoder encoder(){
 		return new BCryptPasswordEncoder();
 	}
 
